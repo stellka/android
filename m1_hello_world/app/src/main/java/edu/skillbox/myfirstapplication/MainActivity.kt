d8f280c68c6c46b4d5610f46f7d590bab018496d
stella_skrylnikova <ssd2001mozdok@yandex.ru>
2022-01-06 15:23:19 +0300

    The first commit.

---
+++ m1_hello_world/app/src/main/java/edu/skillbox/myfirstapplication/MainActivity.kt
@@ -0,0 +1,77 @@
+package edu.skillbox.myfirstapplication
+
+import android.graphics.Color.argb
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.TextView
+import edu.skillbox.myfirstapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.plusOTwoButton.isEnabled = false
+        binding.plusOneButton.setOnClickListener {
+            counter ++
+            binding.botton.text = counter.toString()
+            if (counter==0) {
+                binding.skillboxTextView.text = binding.skillboxTextView.context.getText(R.string.textNameTwo)
+                binding.plusOTwoButton.isEnabled = false
+            }
+            if (counter >=1){
+                binding.skillboxTextView.visibility = View.INVISIBLE
+                binding.skillboxTextViewTwo.visibility = View.VISIBLE
+                binding.count.text = (50-counter).toString()
+
+                binding.plusOTwoButton.isEnabled = true
+            }
+            if (counter in 40..49) {
+                binding.skillboxTextView.visibility = View.INVISIBLE
+                binding.skillboxTextViewTwo.visibility = View.VISIBLE
+                binding.count.text = (49-counter).toString()
+                binding.plusOTwoButton.isEnabled = true
+            }
+            if (counter == 50) {
+                binding.skillboxTextViewTwo.visibility = View.INVISIBLE
+                binding.skillboxTextViewThree.visibility = View.VISIBLE
+                binding.plusOneButton.isEnabled = true
+                binding.plusOTwoButton.isEnabled = true
+                binding.plusThreeButton.visibility = View.VISIBLE
+                binding.count.visibility = View.INVISIBLE
+            }
+        }
+
+        binding.plusOTwoButton.setOnClickListener {
+            if (counter == 0)
+                binding.plusOTwoButton.isEnabled = false
+            else if (counter in 1..50){
+                binding.plusOTwoButton.isEnabled = true
+                counter --
+            }
+            if (counter in 41..49) {
+                binding.skillboxTextViewThree.visibility = View.INVISIBLE
+                binding.skillboxTextViewTwo.visibility = View.VISIBLE
+                counter --
+            }
+            binding.count.text = (50-counter).toString()
+            binding.botton.text = counter.toString()
+        }
+
+        binding.plusThreeButton.setOnClickListener{
+            counter = 0
+            binding.botton.text = counter.toString()
+            binding.plusOneButton.isEnabled = true
+            binding.plusOTwoButton.isEnabled = false
+            binding.plusThreeButton.visibility = View.INVISIBLE
+            binding.skillboxTextViewThree.visibility = View.INVISIBLE
+            binding.skillboxTextView.visibility = View.VISIBLE
+            binding.skillboxTextView.text = binding.skillboxTextView.context.getText(R.string.textNameTwo)
+        }
+
+    }
+}
