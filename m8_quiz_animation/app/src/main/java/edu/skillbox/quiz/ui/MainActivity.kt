f65f959a9cb71b8d33e6860f9f410bfec5395ddb
stella_skrylnikova <ssd2001mozdok@yandex.ru>
2022-06-01 14:13:28 +0300

    The first commit.

---
+++ m8_quiz_animation/app/src/main/java/edu/skillbox/quiz/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package edu.skillbox.quiz.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.skillbox.quiz.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
